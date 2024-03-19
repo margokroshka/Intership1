20cca666a53a0f6341c714f79a6de340e235fdef
margaret <m.i.baranovskaya13@gmail.com>
2024-03-19 22:32:36 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+Q
+!src/test/java/FileSystemTest.java,9/f/9f93a1b6cccdbffaf1f7ffb76f8400c8182349e9
+Z
+*src/main/java/baranovskaya/FileSystem.java,0/5/054f7b7728cecf64562a66a1d024ecb72546b559
+[
++src/main/java/baranovskaya/PathBuilder.java,9/b/9bf5a44b2f77ec804cb218c5027bb72fc4c0e88d
+V
+&src/main/java/baranovskaya/Folder.java,d/9/d9d239894f8190daec7d60c4eeecffa059f6a59c
+T
+$src/main/java/baranovskaya/Item.java,1/e/1e4dc827df0be5b15ac4fff15ab7fdf2edcc446a
---
+++ src/test/java/FileSystemTest.java
@@ -0,0 +1,51 @@
+import baranovskaya.File;
+import baranovskaya.Folder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static baranovskaya.FileSystem.root;
+import static baranovskaya.PathBuilder.processInput;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSystemTest {
+    @Test
+    void testProcessInput_ShouldOutputTheFilesystem_WhenTheUserEntersValidValue() throws Exception {
+        String input = "folder1/folder2/file.txt";
+        processInput(input);
+        Assertions.assertTrue(root.getItems().containsKey("folder1"));
+        Folder folder1 = (Folder) root.getItems().get("folder1");
+        Assertions.assertTrue(folder1.getItems().containsKey("folder2"));
+    }
+
+    @Test
+     void testProcessInput_OutputShouldBeRejected_WhenTheUserEntersInvalidValue() throws Exception {
+        String input = "folder1/folder2/file";
+        Throwable thrown = assertThrows(Exception.class, () -> {
+           processInput(input);
+        });
+        assertNotNull(thrown.getMessage());
+    }
+    @Test
+     void testAddFolder_ShouldBeAdded_WhenTheUserEntersValue() {
+        Folder folder = new Folder("testFolder");
+        assertNotNull(folder);
+        assertEquals("testFolder", folder.getName());
+    }
+
+    @Test
+     void testAddFile_ShouldBeAdded_WhenTheUserEntersValue() {
+        File file = new File("testFile", "txt");
+        assertNotNull(file);
+        assertEquals("testFile.txt", file.toString());
+    }
+
+    @Test
+     void testAddItemToFolder_ShouldBeAdded_WhenTheUserEntersValue() {
+        Folder folder = new Folder("testFolder");
+        File file = new File("testFile", "txt");
+        folder.addItem(file);
+        assertTrue(folder.getItems().containsKey("testFile"));
+        assertEquals(file, folder.getItems().get("testFile"));
+    }
+
+}
